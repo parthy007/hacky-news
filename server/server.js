6b37070bf395136eff72357d72506fad9451aa01
Parth <parthadhikari1812@gmail.com>
2024-02-03 01:22:37 +0530

    cors config

--- server/server.js
+++ server/server.js
@@ -20,6 +20,12 @@ mongoose.connect(process.env.MONGO_URL)
 .catch(err=>console.log(err))
 
 app.use(express.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "OPTIONS, GET, POST");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type");
+    next();
+  });
 app.use("/auth",authRoute);
 
 const PORT = process.env.PORT || 3001;
