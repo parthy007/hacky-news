0a54c26d0acf4aac3518302da1169ca642a527e9
Parth <parthadhikari1812@gmail.com>
2024-02-03 02:47:56 +0530

    cors config

--- server/server.js
+++ server/server.js
@@ -3,34 +3,27 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const authRoute = require('./routes/auth');
 const cors = require('cors');
+const helmet = require('helmet');
 
 const app = express();
 
-// CORS configuration
-const allowedOrigins = ['https://hacky-news-sigma.vercel.app', 'http://localhost:3000'];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: ['OPTIONS', 'GET', 'POST'],
-    credentials: true,
-  })
-);
-
 dotenv.config();
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(express.json());
+app.use(cors({
+    origin: ["https://budssss.vercel.app","http://localhost:3000"],
+    methods: ['GET', 'POST', 'PATCH'],
+    allowedHeaders: ['Content-Type','Authorization','OPTIONS','ORIGIN'],
+}));
+
 
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log('DB is connected'))
   .catch((err) => console.log(err));
 
-app.use(express.json());
+
 app.use('/auth', authRoute);
 
 const PORT = process.env.PORT || 3001;
