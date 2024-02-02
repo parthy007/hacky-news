3b5bfe8a8975cbf7fea9240da23dcb8fc9bf6f9b
Parth <parthadhikari1812@gmail.com>
2024-02-03 00:42:01 +0530

    Cors config

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors({
-    origin:"*",
+    origin:["https://hacky-news-sigma.vercel.app","http://localhost:3000"],
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'OPTIONS', 'ORIGIN'],
     credentials:true,
