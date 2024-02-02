b89c7c2223c649b49c7c8ed013b1713dbb3fa425
Parth <parthadhikari1812@gmail.com>
2024-02-02 17:35:32 +0530

    Cors config

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express();
 dotenv.config()
 
 app.use(cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000","https://hacky-news-sigma.vercel.app"],
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'OPTIONS', 'ORIGIN'],
     credentials: true
